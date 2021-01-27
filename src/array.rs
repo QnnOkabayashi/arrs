086c4d6aa585e75571705288969e5f843f5e816e
Quinn Okabayashi <qokabay1@swarthmore.edu>
2021-01-27 00:39:29 -0800

    Worked on casting

--- src/array.rs
+++ src/array.rs
@@ -6,7 +6,7 @@ mod nestedlist;
 mod result;
 mod shape;
 use cursor::ArrayCursor;
-use dtype::{DType, TypeConscious};
+use dtype::{DType, TypeAware};
 use indexitem::IndexItem;
 use iter::{ArrayIntoIterator, ArrayIterator, ArrayIteratorMut};
 use nestedlist::NestedList;
@@ -14,29 +14,31 @@ use result::{ArrayError, ArrayResult};
 use shape::Shape;
 use std::{convert, fmt, fs, io, mem, rc};
 
-type Data<T> = rc::Rc<Vec<T>>;
-
 pub struct Array<T>
 where
-    T: TypeConscious,
+    T: TypeAware,
 {
     shape: Shape,
-    data: Data<T>, // todo!("make data its own struct?")
+    data: Vec<T>,
 }
 
 impl<T> Array<T>
 where
-    T: TypeConscious,
+    T: TypeAware,
 {
     fn operate<F, R>(&self, rhs: &Self, op: F) -> ArrayResult<R>
     where
-        F: Fn((&T, &T)) -> R,
-        R: TypeConscious,
+        F: Fn(T, T) -> R,
+        R: TypeAware,
     {
         match self.shape().cast(rhs.shape()) {
             Ok(shape) => {
-                let mut data = Vec::with_capacity(shape.volume());
+                let mut data = Vec::with_capacity(shape.volume() as usize);
 
+                // this seems like such a naive solution
+                let lhs_cursor = self.get_data_cursor();
+                let rhs_cursor = rhs.get_data_cursor();
+                
                 // TODO: implement once I know what I'm doing
 
                 // 4 x 1 . 1 x 3 => 4 x 3
@@ -59,7 +61,78 @@ where
                 // but I hate to have to create a struct every time...
                 // create a ArrayDimIterator struct that's super bare bones
 
+                fn operate_rec<T, F, R>(lhs: ArrayElement<T>, rhs: ArrayElement<T>, out: &mut Vec<R>, op: F)
+                where
+                    T: TypeAware,
+                    R: TypeAware,
+                    F: Fn(T, T) -> R,
+                {
+                    if let (ArrayElement::Value(a), ArrayElement::Value(b)) = (lhs, rhs) {
+                        out.push(op(a, b));
+                    }
+                }
                 /*
+                Possibilities:
+                Value and Value
+                Array and Value
+                Arrays of the same rank and same len
+                Arrays of the same rank and different len
+                Arrays of different rank
+
+                I can probably come up with some way that involves destructuring the enum
+                first to be more efficient rather than treating them all the same
+
+                // NEW ALGO PSEUDO CODE
+                value & value => base case
+                if same rank {
+                    // this if statement could probably be compressed
+                    if same len {
+                        // linear case
+                    } else {
+                        // stretch where one is 1
+                    }
+                } else {
+                    stretch 1 into n (Array and Value + Arrays of different rank)
+                }
+
+                // NEW ALGO
+                fn recursive(a: ArrayElement<T>, b: ArrayElement<T>, vec: &mut Vec<R>) {
+                    if let (Value(a), Value(b)) = (a, b) {
+                        // base case
+                    } else if a.ndims() == b.ndims() {
+                        if a.len() == b.len() {
+                            // linear case
+                            for n in ..a.len() {
+                                recursive(a.slice(n), b.slice(n), vec);
+                            }
+                        } else {
+                            // stretch where one is 1
+                            // this could also probably be simplified
+                            if b.len() == 1 {
+                                for n in ..a.len() {
+                                    recursive(a.slice(n), b.slice(0), vec);
+                                }
+                            } else {
+                                for n in ..b.len() {
+                                    recursive(a.slice(0), b.slice(n), vec);
+                                }
+                            }
+                        }
+                    } else {
+                        // varying ndims (this can probably be simplified)
+                        if b.len() == 1 {
+                            for n in ..a.len() {
+                                recursive(a.slice(n), b, vec);
+                            }
+                        } else {
+                            for n in ..b.len() {
+                                recursive(a, b.slice(n), vec);
+                            }
+                        }
+                    }
+                }
+
+                // OLD ALGO
                 fn recursive(a: Array<T>, b: Array<T>, vec: &mut Vec<R>) {
                     if their ranks are equal {
                         if their ranks are 0 {
@@ -87,7 +160,7 @@ where
                         for n in ..N {
                             recursive(a.slice(n), b, vec);
                             // this might be wonky with ownership of b...
-                            // also I'm not sure about if this will mess 
+                            // also I'm not sure about if this will mess
                             // with the ordering of pushing to the result vec
                         }
                     }
@@ -95,10 +168,7 @@ where
                 }
                 */
 
-                Ok(Array {
-                    shape,
-                    data: rc::Rc::new(data),
-                })
+                Ok(Array { shape, data })
             }
             Err(e) => Err(ArrayError::Cast(e)),
         }
@@ -116,7 +186,7 @@ where
         T::Type::new()
     }
 
-    pub fn data(&self) -> &Data<T> {
+    pub fn data(&self) -> &Vec<T> {
         &self.data
     }
 
@@ -133,20 +203,28 @@ where
         todo!("create a T iterator")
     }
 
-    // slice
-    // todo!("figure out semantics of slicing")
+    fn get_data_cursor(&self) -> ArrayCursor<T> {
+        ArrayCursor::new(&self.data)
+    }
+
+    // fn get_dim_cursor(&'a self) -> ArrayDimCursor<T> {
+    //     ArrayDimCursor {
+    //         shape: self.shape(),
+    //         depth: self.rank() - 1,
+    //         index: 0,
+    //         cursor: self.get_data_cursor(),
+    //     }
+    // }
 }
 
 impl<T> Array<T>
 where
-    T: TypeConscious + Copy,
+    T: TypeAware + Copy,
 {
     pub fn reshape(self, shape: Shape) -> ArrayResult<T> {
         if self.shape.volume() == shape.volume() {
-            Ok(Array {
-                shape,
-                data: rc::Rc::new(self.iter().copied().collect()),
-            })
+            let data = self.iter().copied().collect();
+            Ok(Array { shape, data })
         } else {
             Err(ArrayError::Reshape(self.shape().clone(), shape))
         }
@@ -167,7 +245,7 @@ macro_rules! impl_array_cmp {
     { $( $name:ident: $e:expr ),* } => {
         impl<T> Array<T>
         where
-            T: TypeConscious + PartialOrd,
+            T: TypeAware + PartialOrd,
         {
             $(
                 pub fn $name(&self, rhs: &Self) -> ArrayResult<bool> {
@@ -179,12 +257,12 @@ macro_rules! impl_array_cmp {
 }
 
 impl_array_cmp! {
-    eq: |(a, b)| a == b,
-    ne: |(a, b)| a != b,
-    lt: |(a, b)| a < b,
-    le: |(a, b)| a <= b,
-    gt: |(a, b)| a > b,
-    ge: |(a, b)| a >= b
+    eq: |a, b| a == b,
+    ne: |a, b| a != b,
+    lt: |a, b| a < b,
+    le: |a, b| a <= b,
+    gt: |a, b| a > b,
+    ge: |a, b| a >= b
 }
 
 macro_rules! impl_array_astype {
@@ -209,13 +287,13 @@ macro_rules! impl_array_astype {
                 }
             }
 
-            impl TypeConscious for $inner_type {
+            impl TypeAware for $inner_type {
                 type Type = $type_struct;
             }
 
             impl<T> Array<T>
             where
-                T: TypeConscious + Copy + PartialOrd + convert::Into<$inner_type>,
+                T: TypeAware + Copy + PartialOrd + convert::Into<$inner_type>,
             {
                 pub fn $name(&self) -> Array<$inner_type> {
                     let data = self
@@ -225,7 +303,7 @@ macro_rules! impl_array_astype {
 
                     Array {
                         shape: self.shape.clone(),
-                        data: rc::Rc::new(data),
+                        data,
                     }
                 }
             }
@@ -243,64 +321,71 @@ impl_array_astype! {
     astype_float64 for Float64 as f64
 }
 
-
-
 // iterators over each dimension
 // would be nice if I made pseudo-shapes so
 // that they match up nicely (equal size, 1-padded)
 // a struct where you give it an index, and it gives you
 // an iterator for that dim
 
+// rename to "CastingSlice" or something
+// so it's clear that it "stretches" if it's a Value varient
 pub enum ArrayElement<'a, T>
 where
-    T: TypeConscious,
+    T: TypeAware,
 {
-    Array(ArrayDimIterator<'a, T>),
-    Value(&'a T),
+    Array {
+        shape: &'a Shape,
+        ndims: usize,
+        pos: isize,
+        cursor: &'a mut ArrayCursor<'a, T>,
+    },
+    Value(T), // holds a copied value
 }
 
-pub struct ArrayDimIterator<'a, T>
+impl<'a, T> ArrayElement<'a, T>
 where
-    T: TypeConscious,
+    T: TypeAware,
 {
-    shape: &'a Shape,
-    depth: usize,
-    index: usize,
-    data: &'a mut ArrayCursor<'a, T>,
-}
-
-impl<'a, T> Iterator for ArrayDimIterator<'a, T>
-where
-    T: TypeConscious,
-{
-    type Item = ArrayElement<'a, T>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.index < self.shape.dim(self.depth)? {
-            Some(if self.depth == 0 {
-                let value = ArrayElement::Value(self.data.read());
-                self.data.move_pos(1);
-                self.index += 1;
-                value
-            } else {
-                let array = ArrayElement::Array(
-                    ArrayDimIterator {
-                        shape: self.shape,
-                        depth: self.depth - 1,
-                        index: 0,
-                        data: self.data,
-                    }
-                );
-                array
-            })
-        } else {
-            None
+    fn ndims(&self) -> usize {
+        match self {
+            Self::Array { ndims, .. } => *ndims,
+            Self::Value(_) => 0,
         }
     }
 
+    fn len(&self) -> isize {
+        match self {
+            Self::Array { shape, ndims, .. } => shape.dim(ndims - 1),
+            Self::Value(_) => 1,
+        }
+    }
+
+    fn at(&'a self, index: isize) -> Option<ArrayElement<'a, T>> {
+        match self {
+            Self::Array { shape, ndims, cursor , .. } => {
+                if index < shape.dim(ndims - 1) {
+                    None
+                    // Some(if *ndims == 0 {
+                    //     ArrayElement::Value(cursor.read())
+                    // } else {
+                    //     ArrayElement::Array {
+                    //         shape,
+                    //         ndims: ndims - 1,
+                    //         pos: 0,
+                    //         cursor: *cursor,
+                    //     }
+                    // })
+                } else {
+                    None
+                }
+            },
+            Self::Value(_) => {
+                None
+                // Some(self)
+            },
+        }
+    }
 }
-
-
 
 
 #[test]
@@ -310,18 +395,17 @@ fn test_compile() {
 
 impl<T> convert::From<Vec<T>> for Array<T>
 where
-    T: TypeConscious,
+    T: TypeAware,
 {
     fn from(data: Vec<T>) -> Self {
         let shape = Shape::new(vec![1]);
-        let data = rc::Rc::new(data);
         Self { shape, data }
     }
 }
 
 impl<T> convert::TryFrom<NestedList<T>> for Array<T>
 where
-    T: TypeConscious,
+    T: TypeAware,
 {
     type Error = ArrayError<T>;
 
@@ -332,7 +416,7 @@ where
 
 impl<T> convert::TryFrom<fs::File> for Array<T>
 where
-    T: TypeConscious,
+    T: TypeAware,
 {
     type Error = ArrayError<T>;
 
--- src/array/cursor.rs
+++ src/array/cursor.rs
@@ -1,33 +1,42 @@
-use super::dtype::TypeConscious;
+use super::dtype::TypeAware;
 
-// only make one cursor and pass it around between ArrayDimIterator's
-// each one is constructed whenever you need to do stuff with the data
+// A basic implementation of an immutable cursor.
+// Improve later; this is just for testing purposes
+// and can probably be improved by using the raw pointer
+// but that's not a priority right now.
+// probably using Vec::as_ptr()
 pub struct ArrayCursor<'a, T>
 where
-    T: TypeConscious
+    T: TypeAware,
 {
     inner: &'a Vec<T>,
-    pos: usize,
+    pos: isize,
 }
 
 impl<'a, T> ArrayCursor<'a, T>
 where
-    T: TypeConscious,
+    T: TypeAware,
 {
     pub fn new(inner: &'a Vec<T>) -> Self {
         Self { inner, pos: 0 }
     }
 
-    pub fn move_pos(&mut self, len: usize) {
-        // TODO: add error checking here
-        self.pos += len
-    }
-
-    pub fn pos(&self) -> usize {
+    pub fn get_pos(&self) -> isize {
         self.pos
     }
 
-    pub fn read(&self) -> &T {
-        &self.inner[self.pos]
+    pub fn set_pos(&mut self, pos: isize) {
+        self.pos = pos
+    }
+
+    pub fn move_pos(&mut self, offset: isize) {
+        self.set_pos(self.pos + offset)
+    }
+
+    // panics if the index is out of bounds
+    pub fn read(&self) -> T {
+        // TODO: figure out OutOfBounds indexing
+        // performs a copy here
+        self.inner[self.pos as usize]
     }
 }
