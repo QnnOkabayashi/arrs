52bebcca06f882f94fcc7f7965e43e82b5f3b485
Quinn Okabayashi <qokabay1@swarthmore.edu>
2021-01-27 15:52:24 -0800

    Cleaned up

--- src/array/item.rs
+++ src/array/item.rs
@@ -1,4 +1,4 @@
-use super::{Array, TypeAware, Shape, convert::TryInto};
+use super::{convert::TryInto, Array, Shape, TypeAware};
 
 #[derive(Clone, Copy)]
 pub struct Item<'a, T>
@@ -33,8 +33,6 @@ where
     }
 
     fn stride(&self) -> isize {
-        // NOT THE DIM BELOW, BUT THE PRODUCT OF ALL THE SMALLER DIMS
-        // self.shape.dim(self.ndims() - 2)
         let ndims = (self.ndims - 1).try_into().unwrap_or(0);
         self.shape.iter().take(ndims).product()
     }
@@ -59,32 +57,4 @@ where
     pub fn read(&self) -> T {
         self.data[self.offset as usize]
     }
-
-    // TODO: remake this to support ArrayElement in struct form (not enum)
-    // fn at(&'a self, index: isize) -> Option<ArrayElement<'a, T>> {
-    //     match self {
-    //         Self::Array { shape, ndims, cursor , .. } => {
-    //             if index < shape.dim(ndims - 1) {
-    //                 None
-    //                 // Some(if *ndims == 0 {
-    //                 //     ArrayElement::Value(cursor.read())
-    //                 // } else {
-    //                 //     ArrayElement::Array {
-    //                 //         shape,
-    //                 //         ndims: ndims - 1,
-    //                 //         pos: 0,
-    //                 //         cursor: *cursor,
-    //                 //     }
-    //                 // })
-    //             } else {
-    //                 None
-    //             }
-    //         },
-    //         Self::Value(_) => {
-    //             None
-    //             // Some(self)
-    //         },
-    //     }
-    // }
 }
-
