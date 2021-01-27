94d3cafcb4fa9c2d7c190c052fca0e5790713418
Quinn Okabayashi <qokabay1@swarthmore.edu>
2021-01-27 00:39:50 -0800

    Added comments describing traits

--- src/array/dtype.rs
+++ src/array/dtype.rs
@@ -1,6 +1,9 @@
-pub trait TypeConscious {
+// impl'd for types that know what type they are
+pub trait TypeAware: Copy {
     type Type: DType;
 }
+
+// impl'd for unit structs containing type info
 pub trait DType: std::fmt::Display + PartialEq + Clone + Copy {
     fn new() -> Self;
 
