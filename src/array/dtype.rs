0afb0b7739735c7a9238c721e68c7af606abf9a2
Quinn Okabayashi <qokabay1@swarthmore.edu>
2021-02-08 18:40:56 -0500

    Added idx format dtype id support

--- src/array/dtype.rs
+++ src/array/dtype.rs
@@ -1,11 +1,14 @@
+use std::fmt;
 // impl'd for types that know what type they are
 pub trait TypeAware: Copy {
     type Type: DType;
 }
 
 // impl'd for unit structs containing type info
-pub trait DType: std::fmt::Display + PartialEq + Clone + Copy {
+pub trait DType: fmt::Display + PartialEq + Clone + Copy {
     fn new() -> Self;
 
-    fn bytes(&self) -> usize;
+    fn bytes() -> usize;
+
+    fn id() -> u8;
 }
