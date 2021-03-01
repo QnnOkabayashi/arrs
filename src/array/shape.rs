2111514a29c6b4a399be0d5b88f16c683e5bd101
Quinn Okabayashi <qokabay1@swarthmore.edu>
2021-03-01 09:46:14 -0500

    Added now Shape type with ShapeBase

--- src/array/shape.rs
+++ src/array/shape.rs
@@ -1,13 +1,214 @@
-use crate::array::{Arc, ArrResult, Error};
+use crate::array::{Arc, ArrResult, Error, PartialView};
 use core::{
     cmp::max,
     fmt::{self, Display},
     iter::once,
 };
 
+pub struct ShapeBase {
+    dims: Vec<usize>,
+    volumes: Vec<usize>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Shape<'a> {
+    len: usize,
+    volume: usize,
+    sub_dims: &'a [usize],
+    sub_volumes: &'a [usize],
+}
+
+impl ShapeBase {
+    pub(self) fn new(dims: Vec<usize>) -> Self {
+        let volumes = once(1)
+            .chain(dims.iter().scan(1, |volume, &dim| {
+                *volume *= dim;
+                Some(*volume)
+            }))
+            .collect();
+
+        Self { dims, volumes }
+    }
+
+    pub(super) fn new_checked(dims: Vec<usize>) -> ArrResult<Self> {
+        if dims.len() == 0 {
+            return Err(Error::ShapeZeroDims);
+        } else if dims.iter().any(|&dim| dim == 0) {
+            return Err(Error::ShapeZeroLenDim { dims });
+        }
+
+        Ok(Self::new(dims))
+    }
+
+    pub(super) fn total_volume(&self) -> usize {
+        *self.volumes.last().unwrap()
+    }
+}
+
+impl<'a> Shape<'a> {
+    pub(self) fn from_parts(
+        len: usize,
+        volume: usize,
+        sub_dims: &'a [usize],
+        sub_volumes: &'a [usize],
+    ) -> Self {
+        Self {
+            len,
+            volume,
+            sub_dims,
+            sub_volumes,
+        }
+    }
+
+    pub(super) fn ndims(&self) -> usize {
+        self.sub_dims.len() + 1
+    }
+
+    pub(super) fn len(&self) -> usize {
+        self.len
+    }
+
+    fn dims_iter(&self) -> impl DoubleEndedIterator<Item = &usize> {
+        self.sub_dims.iter().chain(once(&self.len))
+    }
+
+    pub(super) fn stride(&self) -> usize {
+        // SAFETY: sub_volumes always starts with an extra 1
+        *self.sub_volumes.last().unwrap()
+    }
+
+    fn derank(&self) -> Self {
+        let (&len, sub_dims) = self.sub_dims.split_last().unwrap();
+        let (&volume, sub_volumes) = self.sub_volumes.split_last().unwrap();
+
+        Self::from_parts(len, volume, sub_dims, sub_volumes)
+    }
+
+    fn slice(&self, len: usize) -> Self {
+        let volume = len * self.sub_volumes.last().unwrap();
+
+        Shape::from_parts(len, volume, self.sub_dims, self.sub_volumes)
+    }
+
+    pub(super) fn derank_checked(&self, index: usize) -> ArrResult<Shape> {
+        if self.sub_dims.len() == 0 {
+            return Err(Error::Derank1D);
+        } else if index >= self.len {
+            return Err(Error::DerankIndexOutOfBounds {
+                len: self.len,
+                index,
+            });
+        }
+
+        Ok(self.derank())
+    }
+
+    pub(super) fn slice_checked(&self, start: usize, stop: usize) -> ArrResult<Shape> {
+        if start == stop {
+            return Err(Error::SliceZeroWidth { index: start });
+        } else if start > stop {
+            return Err(Error::SliceStopBeforeStart { start, stop });
+        } else if stop > self.len {
+            return Err(Error::SliceStopPastEnd {
+                stop,
+                dim: self.len,
+            });
+        }
+
+        Ok(self.slice(stop - start))
+    }
+
+    pub(super) fn broadcast(&self, other: &Self) -> ArrResult<(ShapeBase, Vec<BroadcastInstruction>)> {
+        use BroadcastInstruction::*;
+        let result_ndims = max(self.ndims(), other.ndims());
+        let mut dims = Vec::with_capacity(result_ndims);
+        let mut broadcast_instructions = Vec::with_capacity(result_ndims);
+
+        let mut iter1 = self.dims_iter();
+        let mut iter2 = other.dims_iter();
+
+        loop {
+            match (iter1.next(), iter2.next()) {
+                (None, None) => break,
+                (Some(&a), Some(&b)) if a == b => {
+                    broadcast_instructions.push(RecurseLinear);
+                    dims.push(a);
+                }
+                (Some(&a), Some(1)) => {
+                    broadcast_instructions.push(RecurseStretchB);
+                    dims.push(a);
+                }
+                (Some(1), Some(&b)) => {
+                    broadcast_instructions.push(RecurseStretchA);
+                    dims.push(b);
+                }
+                (Some(&a), None) => {
+                    broadcast_instructions.push(RecursePadB);
+                    dims.push(a);
+                }
+                (None, Some(&b)) => {
+                    broadcast_instructions.push(RecursePadA);
+                    dims.push(b);
+                }
+                _ => {
+                    return Err(Error::Broadcast {
+                        dims1: self.dims_iter().rev().copied().collect(),
+                        dims2: other.dims_iter().rev().copied().collect(),
+                    })
+                }
+            }
+        }
+
+        broadcast_instructions[0] = match broadcast_instructions[0] {
+            RecurseLinear => PushLinear,
+            RecurseStretchA => PushStretchA,
+            RecurseStretchB => PushStretchB,
+            _ => panic!("This pattern is unreachable because it would require a shape with zero dims"),
+        };
+
+        Ok((ShapeBase::new(dims), broadcast_instructions))
+    }
+}
+
+impl<'base> PartialView<'base> for Shape<'base> {
+    type Base = ShapeBase;
+
+    fn from_base(base: &'base Self::Base) -> Self {
+        let (&len, sub_dims) = base.dims.split_last().unwrap();
+        let (&volume, sub_volumes) = base.volumes.split_last().unwrap();
+
+        Self {
+            len,
+            volume,
+            sub_dims,
+            sub_volumes,
+        }
+    }
+
+    fn into_base(&self) -> Self::Base {
+        // recalculating volume is O(n)
+        // copying with a chain iter is O(n)
+        // may as well recalculate
+        ShapeBase::new(self.dims_iter().copied().collect())
+    }
+}
+
+#[derive(Clone, Copy)]
+pub(super) enum BroadcastInstruction {
+    PushLinear,
+    PushStretchA,
+    PushStretchB,
+    RecurseLinear,
+    RecurseStretchA,
+    RecurseStretchB,
+    RecursePadA,
+    RecursePadB,
+}
+
+
 // lowest dimensions are first
 #[derive(Debug, Clone)]
-pub struct Shape {
+pub struct Shape1 {
     ndims: usize,
     dim: usize,
     volume: usize,
@@ -15,7 +216,7 @@ pub struct Shape {
     volumes: Arc<Vec<usize>>,
 }
 
-impl Shape {
+impl Shape1 {
     pub fn new(dims: Vec<usize>) -> Self {
         assert!(dims.len() > 0, "shapes can't have 0 dims");
         assert!(
@@ -38,6 +239,7 @@ impl Shape {
 
         let volumes = Arc::new(volumes_vec);
 
+        // won't panic because dims and volumes both have at least 1 item
         let dim = *dims.last().unwrap();
         let volume = *volumes.last().unwrap();
 
@@ -60,7 +262,7 @@ impl Shape {
         }
     }
 
-    pub fn cast(&self, other: &Shape) -> ArrResult<Shape> {
+    pub fn cast(&self, other: &Shape1) -> ArrResult<Shape1> {
         let mut dims = Vec::with_capacity(max(self.ndims(), other.ndims()) as usize);
 
         let (lhs, rhs) = (self.get_dims(), other.get_dims());
@@ -86,7 +288,7 @@ impl Shape {
         }
 
         // Can volumes be predetermined here? idk
-        Ok(Shape::new(dims))
+        Ok(Shape1::new(dims))
     }
 
     pub fn ndims(&self) -> usize {
@@ -124,11 +326,11 @@ impl Shape {
             .collect()
     }
 
-    pub fn derank(&self, index: usize) -> ArrResult<Shape> {
+    pub fn derank(&self, index: usize) -> ArrResult<Shape1> {
         if self.ndims() == 1 {
             return Err(Error::Derank1D);
         } else if index >= self.dim {
-            return Err(Error::DerankInvalidIndex {
+            return Err(Error::DerankIndexOutOfBounds {
                 len: self.dim,
                 index,
             });
@@ -141,10 +343,10 @@ impl Shape {
         let dims = self.dims.clone();
         let volumes = self.volumes.clone();
 
-        Ok(Shape::from_parts(ndims, dim, volume, dims, volumes))
+        Ok(Shape1::from_parts(ndims, dim, volume, dims, volumes))
     }
 
-    pub fn slice(&self, start: usize, stop: usize) -> ArrResult<Shape> {
+    pub fn slice(&self, start: usize, stop: usize) -> ArrResult<Shape1> {
         if start == stop {
             return Err(Error::SliceZeroWidth { index: start });
         } else if start > stop {
@@ -163,11 +365,11 @@ impl Shape {
         let dims = self.dims.clone();
         let volumes = self.volumes.clone();
 
-        Ok(Shape::from_parts(ndims, dim, volume, dims, volumes))
+        Ok(Shape1::from_parts(ndims, dim, volume, dims, volumes))
     }
 }
 
-impl PartialEq for Shape {
+impl PartialEq for Shape1 {
     fn eq(&self, other: &Self) -> bool {
         self.ndims == other.ndims
             && self.dim == other.dim
@@ -175,7 +377,7 @@ impl PartialEq for Shape {
     }
 }
 
-impl Display for Shape {
+impl Display for Shape1 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:?}", self.get_dims().reverse())
     }
