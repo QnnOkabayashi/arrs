2b4f336a18a77d2e9b290904100064beccd059bc
Quinn Okabayashi <qokabay1@swarthmore.edu>
2021-03-01 09:44:43 -0500

    Added comment on noalias compiler opts for future

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,10 @@ pub mod array;
 pub mod serde_arrs;
 pub mod tests;
 // mod indexing;
+
+/*
+https://stackoverflow.com/a/57259339/12401179
+enable no-aliasing (Fortran style)
+rust nightly: -Zmutable-noalias=yes
+very risky tho, only do it once everything works
+*/
