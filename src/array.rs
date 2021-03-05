0d4d5619cce2042686d20d9df30697b89ba4be74
Quinn Okabayashi <qokabay1@swarthmore.edu>
2021-03-05 00:43:43 -0500

    Consolidated IDX functions into one module

--- src/array.rs
+++ src/array.rs
@@ -2,12 +2,11 @@ mod dense;
 mod error;
 mod shape;
 mod traits;
-use self::traits::IdxType;
 pub use dense::Array as DenseArray;
 pub use error::{ArrResult, Error};
 use shape::BroadcastInstruction;
 pub use shape::{Shape, ShapeBase};
-pub use traits::{Broadcastable, PartialView, TypeAware};
+pub use traits::{MultiDimensional, PartialView, TypeAware};
 
 pub struct ArrayBase<T: TypeAware> {
     shape_base: ShapeBase,
@@ -30,49 +29,95 @@ impl<T: TypeAware> ArrayBase<T> {
 }
 
 #[cfg(not(no_std))]
-impl<T: IdxType> ArrayBase<T> {
-    pub fn from_idx(filename: &'static str) -> ArrResult<Self> {
-        use std::fs::File;
-        use std::io::Read;
+mod idx {
+    use crate::array::{ArrResult, ArrayBase, Error, ShapeBase, TypeAware};
+    use std::fs::File;
+    use std::io::{Read, Write};
 
-        let mut file = File::open(filename)?;
-        let mut magic = [0; 4];
-        if file.read(&mut magic)? < 4 {
-            return Err(Error::IdxReadUnaccepted);
-        }
+    pub trait IdxType: TypeAware {
+        const ID: u8;
 
-        let magic_dtype = magic[2];
-        if magic_dtype != T::ID {
-            return Err(Error::MismatchDTypeIDs {
-                dtype1: T::ID,
-                dtype2: magic_dtype,
-            });
-        }
+        fn read<R: Read>(reader: &mut R) -> ArrResult<Self>;
 
-        let ndims = magic[3] as usize;
-
-        let mut dims = Vec::with_capacity(ndims);
-        for _ in 0..ndims {
-            dims.push(<i32 as IdxType>::read(&mut file)? as usize);
-        }
-
-        let shape_base = ShapeBase::new_checked(dims)?;
-        let volume = shape_base.total_volume();
-
-        let mut data = Vec::with_capacity(volume);
-        for _ in 0..volume {
-            data.push(<T as IdxType>::read(&mut file)?);
-        }
-
-        Ok(Self { shape_base, data })
+        fn write<W: Write>(&self, writer: &mut W) -> ArrResult<()>;
     }
 
-    pub fn into_idx(&self, filename: &'static str) -> ArrResult<()> {
-        unimplemented!(
-            "can't read '{}' because this function isn't implemented",
-            filename
-        )
+    macro_rules! impl_idxtype {
+        { $( ( $inner_type:tt, $size:expr, $id:expr ) ),* } => {
+            $(
+                impl IdxType for $inner_type {
+                    const ID: u8 = $id;
+
+                    fn read<R: Read>(reader: &mut R) -> ArrResult<Self> {
+                        let mut buf = [0; $size];
+                        if reader.read(&mut buf)? < $size {
+                            return Err(Error::IdxReadUnaccepted);
+                        }
+
+                        Ok(Self::from_be_bytes(buf))
+                    }
+
+                    fn write<W: Write>(&self, writer: &mut W) -> ArrResult<()> {
+                        let buf = self.to_be_bytes();
+                        if writer.write(&buf)? < $size {
+                            return Err(Error::IdxWriteUnaccepted);
+                        }
+
+                        Ok(())
+                    }
+                }
+            )*
+        }
+    }
+
+    impl_idxtype! {
+        (u8, 1, 0x08),
+        (i8, 1, 0x09),
+        (i16, 2, 0x0B),
+        (i32, 4, 0x0C),
+        (f32, 4, 0x0D),
+        (f64, 8, 0x0E)
+    }
+
+    impl<T: IdxType> ArrayBase<T> {
+        pub fn from_idx(filename: &'static str) -> ArrResult<Self> {
+            let mut file = File::open(filename)?;
+            let mut magic = [0; 4];
+            if file.read(&mut magic)? < 4 {
+                return Err(Error::IdxReadUnaccepted);
+            }
+
+            let magic_dtype = magic[2];
+            if magic_dtype != T::ID {
+                return Err(Error::MismatchDTypeIDs {
+                    dtype1: T::ID,
+                    dtype2: magic_dtype,
+                });
+            }
+
+            let ndims = magic[3] as usize;
+
+            let mut dims = Vec::with_capacity(ndims);
+            for _ in 0..ndims {
+                dims.push(<i32 as IdxType>::read(&mut file)? as usize);
+            }
+
+            let shape_base = ShapeBase::new_checked(dims)?;
+            let volume = shape_base.total_volume();
+
+            let mut data = Vec::with_capacity(volume);
+            for _ in 0..volume {
+                data.push(<T as IdxType>::read(&mut file)?);
+            }
+
+            Ok(Self { shape_base, data })
+        }
+
+        pub fn into_idx(&self, filename: &'static str) -> ArrResult<()> {
+            unimplemented!(
+                "can't read '{}' because this function isn't implemented",
+                filename
+            )
+        }
     }
 }
-
-// struct SparseArray; // dynamic access patterns
