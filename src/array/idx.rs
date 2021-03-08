b5b5e5fa4a849a516391b28dfe64a2a9cda7f8a1
Quinn Okabayashi <qokabay1@swarthmore.edu>
2021-03-08 01:50:56 -0500

    Fast buffered reading/writing for IDX files

--- src/array/idx.rs
+++ src/array/idx.rs
@@ -1,104 +1,84 @@
 // only compiles when cfg!(not(no_std))
-use crate::array::{ArrResult, ArrayBase, ArrayType, Error, Shape};
+use crate::array::{ArrResult, ArrayBase, Error};
 use std::fs::File;
-use std::io::{Error as IoError, Read, Write};
-
-pub trait IdxType: ArrayType {
-    const ID: u8;
-
-    fn read<R: Read>(reader: &mut R) -> ArrResult<Self>;
-
-    fn write<W: Write>(&self, writer: &mut W) -> ArrResult<()>;
-}
+use std::io::{self, BufReader, BufWriter, Read, Write};
 
 macro_rules! impl_idxtype {
-    { $( ( $inner_type:tt, $size:expr, $id:expr ) ),* } => {
-        $(
-            impl IdxType for $inner_type {
-                const ID: u8 = $id;
+    { $inner_type:ty, $size:expr, $id:expr } => {
+        impl<const NDIMS: usize> ArrayBase<$inner_type, NDIMS> {
+            pub fn from_idx(filename: &'static str) -> ArrResult<Self> {
+                let mut reader = BufReader::new(File::open(filename)?);
 
-                fn read<R: Read>(reader: &mut R) -> ArrResult<Self> {
-                    let mut buf = [0; $size];
-                    if reader.read(&mut buf)? < $size {
+                let mut magic = [0; 4];
+                if reader.read(&mut magic)? < 4 {
+                    return Err(Error::IdxReadUnaccepted);
+                }
+
+                let magic_dtype = magic[2];
+                if magic_dtype != $id {
+                    return Err(Error::IdxMismatchDTypeIDs {
+                        expected: $id,
+                        actual: magic_dtype,
+                    });
+                }
+
+                let magic_ndims = magic[3];
+                if magic_ndims != NDIMS as u8 {
+                    return Err(Error::IdxMismatchNDims {
+                        expected: NDIMS as u8,
+                        actual: magic_ndims,
+                    });
+                }
+
+                let mut dims = [0; NDIMS];
+                for dim in dims.iter_mut() {
+                    let mut bytes = [0; 4];
+                    if reader.read(&mut bytes)? < 4 {
                         return Err(Error::IdxReadUnaccepted);
                     }
-
-                    Ok(Self::from_be_bytes(buf))
+                    *dim = i32::from_be_bytes(bytes) as usize;
                 }
 
-                fn write<W: Write>(&self, writer: &mut W) -> ArrResult<()> {
-                    let buf = self.to_be_bytes();
-                    if writer.write(&buf)? < $size {
-                        return Err(Error::IdxWriteUnaccepted);
+                let mut data = Vec::with_capacity(dims.iter().product());
+                for _ in 0..data.capacity() {
+                    let mut bytes = [0; $size];
+                    if reader.read(&mut bytes)? < $size {
+                        return Err(Error::IdxReadUnaccepted);
                     }
-
-                    Ok(())
+                    data.push(<$inner_type>::from_be_bytes(bytes));
                 }
+
+                Ok(Self { dims, data })
             }
-        )*
+
+            pub fn into_idx(self, filename: &'static str) -> ArrResult<()> {
+                let mut writer = BufWriter::new(File::create(filename)?);
+
+                writer.write(&[0, 0, $id, NDIMS as u8])?;
+
+                for &dim in self.dims.iter() {
+                    writer.write(&(dim as i32).to_be_bytes())?;
+                }
+
+                for &value in self.data.iter() {
+                    writer.write(&value.to_be_bytes())?;
+                }
+
+                Ok(())
+            }
+        }
     }
 }
 
-impl_idxtype! {
-    (u8, 1, 0x08),
-    (i8, 1, 0x09),
-    (i16, 2, 0x0B),
-    (i32, 4, 0x0C),
-    (f32, 4, 0x0D),
-    (f64, 8, 0x0E)
-}
+impl_idxtype! { u8, 1, 0x08 }
+impl_idxtype! { i8, 1, 0x09 }
+impl_idxtype! { i16, 2, 0x0B }
+impl_idxtype! { i32, 4, 0x0C }
+impl_idxtype! { f32, 4, 0x0D }
+impl_idxtype! { f64, 8, 0x0E }
 
-impl<T: IdxType, const NDIMS: usize> ArrayBase<T, NDIMS> {
-    pub fn from_idx(filename: &'static str) -> ArrResult<Self> {
-        let mut file = File::open(filename)?;
-        let mut magic = [0; 4];
-
-        if file.read(&mut magic)? < 4 {
-            return Err(Error::IdxReadUnaccepted);
-        }
-
-        let magic_dtype = magic[2];
-        if magic_dtype != T::ID {
-            return Err(Error::IdxMismatchDTypeIDs {
-                expected: T::ID,
-                actual: magic_dtype,
-            });
-        }
-
-        let magic_ndims = magic[3];
-        if magic_ndims != NDIMS as u8 {
-            return Err(Error::IdxMismatchNDims {
-                expected: NDIMS as u8,
-                actual: magic_ndims,
-            });
-        }
-
-        let mut dims = [0; NDIMS];
-
-        for dim in dims.iter_mut() {
-            *dim = <i32 as IdxType>::read(&mut file)? as usize;
-        }
-
-        let shape = Shape::new(dims);
-
-        let mut data = Vec::with_capacity(shape.volume());
-        for _ in 0..data.capacity() {
-            data.push(<T as IdxType>::read(&mut file)?);
-        }
-
-        Ok(Self { shape, data })
-    }
-
-    pub fn into_idx(&self, filename: &'static str) -> ArrResult<()> {
-        unimplemented!(
-            "can't read '{}' because this function isn't implemented",
-            filename
-        )
-    }
-}
-
-impl From<IoError> for Error {
-    fn from(err: IoError) -> Self {
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Self {
         Self::IdxIO {
             message: err.to_string(),
         }
