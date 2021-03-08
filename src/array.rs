cd87b0639343aa8385b8c14c49045217c8a2ff58
Quinn Okabayashi <qokabay1@swarthmore.edu>
2021-03-08 01:50:18 -0500

    Consolidated Shape type into Array

--- src/array.rs
+++ src/array.rs
@@ -1,15 +1,13 @@
 mod error;
 #[cfg(not(no_std))]
 mod idx;
-mod shape;
+// mod shape;
 #[macro_use]
 mod macros;
 use core::fmt::Debug;
 use core::iter::{repeat, Sum};
 use core::ops::{Add, Div, Mul, Sub};
 pub use error::{ArrResult, Error};
-use shape::BroadcastInstruction;
-pub use shape::Shape;
 
 // helper function for compile time use
 pub const fn max_const(v1: usize, v2: usize) -> usize {
@@ -29,34 +27,32 @@ pub const fn min_const(v1: usize, v2: usize) -> usize {
 }
 
 /// A base for owning `Array` data
-pub struct ArrayBase<T: ArrayType, const NDIMS: usize> {
-    shape: Shape<NDIMS>,
+pub struct ArrayBase<T: ArrType, const NDIMS: usize> {
+    dims: [usize; NDIMS],
     data: Vec<T>,
 }
 
-impl<T: ArrayType, const NDIMS: usize> ArrayBase<T, NDIMS> {
-    pub fn new(shape: Shape<NDIMS>, data: Vec<T>) -> ArrResult<Self> {
-        let volume = shape.volume();
-
-        if volume != data.len() {
-            return Err(Error::ShapeDataMisalignment {
-                volume,
-                data_len: data.len(),
-            });
+impl<T: ArrType, const NDIMS: usize> ArrayBase<T, NDIMS> {
+    pub fn new(dims: [usize; NDIMS], data: Vec<T>) -> ArrResult<Self> {
+        let (volume, len) = (dims.iter().product(), data.len());
+        if NDIMS == 0 {
+            Err(Error::ShapeZeroDims)
+        } else if volume != len {
+            Err(Error::ShapeDataMisalignment { volume, len })
+        } else {
+            Ok(Self { dims, data })
         }
-
-        Ok(Self { shape, data })
     }
 }
 
 /// A view into an `ArrayBase` object
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub struct Array<'base, T: ArrayType, const NDIMS: usize> {
-    shape: Shape<NDIMS>,
+pub struct Array<'base, T: ArrType, const NDIMS: usize> {
+    dims: [usize; NDIMS],
     data: &'base [T],
 }
 
-impl<'base, T: ArrayType, const NDIMS: usize> Array<'base, T, NDIMS> {
+impl<'base, T: ArrType, const NDIMS: usize> Array<'base, T, NDIMS> {
     /// Combine `Array`s of different sizes using array broadcasting
     pub fn broadcast_combine<const NDIMS2: usize, F>(
         &self,
@@ -66,69 +62,103 @@ impl<'base, T: ArrayType, const NDIMS: usize> Array<'base, T, NDIMS> {
     where
         F: Fn(T, T) -> T,
     {
-        match self.shape.broadcast(&other.shape) {
-            Ok((shape, broadcast_instructions)) => {
-                use BroadcastInstruction::*;
-                let mut data = Vec::with_capacity(shape.volume());
+        #[derive(Clone, Copy)]
+        pub enum Instruction {
+            PushLinear,
+            PushStretchA,
+            PushStretchB,
+            RecurseLinear { stride_a: usize, stride_b: usize },
+            RecurseStretchA { stride_b: usize },
+            RecurseStretchB { stride_a: usize },
+        }
+        use Instruction::*;
 
-                fn recurse<'base1, T, F>(
-                    a: &[T],
-                    b: &[T],
-                    instructions: &[BroadcastInstruction],
-                    out: &mut Vec<T>,
-                    f: &F,
-                ) where
-                    T: ArrayType,
-                    F: Fn(T, T) -> T,
-                {
-                    let (instruction, instructions2) = instructions.split_last().unwrap();
+        let (dims, instructions) = {
+            let mut dims = [0; max_const(NDIMS, NDIMS2)];
+            let mut instructions = [PushLinear; max_const(NDIMS, NDIMS2)];
 
-                    match *instruction {
-                        PushLinear => {
-                            out.extend(a.iter().zip(b.iter()).map(|(&a_n, &b_n)| f(a_n, b_n)));
-                        }
-                        PushStretchA => {
-                            out.extend(b.iter().map(|&b_n| f(a[0], b_n)));
-                        }
-                        PushStretchB => {
-                            out.extend(a.iter().map(|&a_n| f(a_n, b[0])));
-                        }
-                        RecurseLinear { stride_a, stride_b } => {
-                            for (a2, b2) in a.chunks_exact(stride_a).zip(b.chunks_exact(stride_b)) {
-                                recurse(a2, b2, instructions2, out, f);
-                            }
-                        }
-                        RecurseStretchA { stride_b } => {
-                            for b2 in b.chunks_exact(stride_b) {
-                                recurse(a, b2, instructions2, out, f);
-                            }
-                        }
-                        RecurseStretchB { stride_a } => {
-                            for a2 in a.chunks_exact(stride_a) {
-                                recurse(a2, b, instructions2, out, f);
-                            }
-                        }
+            let (mut iter_a, mut iter_b) = (self.dims.iter(), other.dims.iter());
+            let (mut stride_a, mut stride_b) = (1, 1);
+
+            for (dim, instruction) in dims.iter_mut().zip(instructions.iter_mut()) {
+                let (next_a, next_b) = (iter_a.next(), iter_b.next());
+
+                let (d, i) = match (next_a, next_b) {
+                    (Some(&a), Some(&b)) if a == b => (a, RecurseLinear { stride_a, stride_b }),
+                    (Some(&a), Some(1)) | (Some(&a), None) => (a, RecurseStretchB { stride_a }),
+                    (Some(1), Some(&b)) | (None, Some(&b)) => (b, RecurseStretchA { stride_b }),
+                    (None, None) => unreachable!(),
+                    _ => {
+                        return Err(Error::Broadcast {
+                            dims1: self.dims.to_vec(),
+                            dims2: other.dims.to_vec(),
+                        })
+                    }
+                };
+
+                stride_a *= next_a.unwrap_or(&1);
+                stride_b *= next_b.unwrap_or(&1);
+
+                *dim = d;
+                *instruction = i;
+            }
+
+            instructions[0] = match instructions[0] {
+                RecurseLinear { .. } => PushLinear,
+                RecurseStretchA { .. } => PushStretchA,
+                RecurseStretchB { .. } => PushStretchB,
+                _ => unreachable!(),
+            };
+
+            (dims, instructions)
+        };
+
+        let mut data = Vec::with_capacity(dims.iter().product());
+
+        fn recurse<T, F>(a: &[T], b: &[T], instructions: &[Instruction], out: &mut Vec<T>, f: &F)
+        where
+            T: ArrType,
+            F: Fn(T, T) -> T,
+        {
+            let (instruction, instructions2) = instructions.split_last().unwrap();
+
+            match *instruction {
+                PushLinear => {
+                    out.extend(a.iter().zip(b.iter()).map(|(&a_n, &b_n)| f(a_n, b_n)));
+                }
+                PushStretchA => {
+                    out.extend(b.iter().map(|&b_n| f(a[0], b_n)));
+                }
+                PushStretchB => {
+                    out.extend(a.iter().map(|&a_n| f(a_n, b[0])));
+                }
+                RecurseLinear { stride_a, stride_b } => {
+                    for (a2, b2) in a.chunks_exact(stride_a).zip(b.chunks_exact(stride_b)) {
+                        recurse(a2, b2, instructions2, out, f);
                     }
                 }
-
-                recurse(
-                    self.data,
-                    other.data,
-                    &broadcast_instructions,
-                    &mut data,
-                    &combinator,
-                );
-
-                Ok(ArrayBase { shape, data })
+                RecurseStretchA { stride_b } => {
+                    for b2 in b.chunks_exact(stride_b) {
+                        recurse(a, b2, instructions2, out, f);
+                    }
+                }
+                RecurseStretchB { stride_a } => {
+                    for a2 in a.chunks_exact(stride_a) {
+                        recurse(a2, b, instructions2, out, f);
+                    }
+                }
             }
-            Err(e) => Err(e),
         }
+
+        recurse(self.data, other.data, &instructions, &mut data, &combinator);
+
+        Ok(ArrayBase { dims, data })
     }
 
     /// Convert the data type
-    pub fn as_type<R: ArrayType + From<T>>(&self) -> ArrayBase<R, NDIMS> {
+    pub fn as_type<R: ArrType + From<T>>(&self) -> ArrayBase<R, NDIMS> {
         ArrayBase {
-            shape: self.shape,
+            dims: self.dims,
             data: self.data.iter().map(|x| R::from(*x)).collect(),
         }
     }
@@ -136,7 +166,7 @@ impl<'base, T: ArrayType, const NDIMS: usize> Array<'base, T, NDIMS> {
     /// Generate fresh `Array` from an `ArrayBase`
     pub fn from_base(base: &'base ArrayBase<T, NDIMS>) -> Self {
         Self {
-            shape: base.shape,
+            dims: base.dims,
             data: &base.data[..],
         }
     }
@@ -144,13 +174,9 @@ impl<'base, T: ArrayType, const NDIMS: usize> Array<'base, T, NDIMS> {
     /// Generate fresh `ArrayBase` from this `Array`
     pub fn into_base(&self) -> ArrayBase<T, NDIMS> {
         ArrayBase {
-            shape: self.shape,
+            dims: self.dims,
             data: self.data.to_vec(),
         }
-    }
-
-    pub fn shape(&self) -> &Shape<NDIMS> {
-        &self.shape
     }
 
     /// Matrix multiplication for 2x2, 2x1, 1x2, and 1x1 `Array`s
@@ -172,8 +198,8 @@ impl<'base, T: ArrayType, const NDIMS: usize> Array<'base, T, NDIMS> {
             }
             (2, 1) => {
                 // matrix vector
-                let (rows_a, cols_a) = (self.shape.dims()[0], self.shape.dims()[1]);
-                let len_b = other.shape.dims()[0];
+                let (rows_a, cols_a) = (self.dims[0], self.dims[1]);
+                let len_b = other.dims[0];
                 if rows_a != len_b {
                     return Err(Error::MatMul {
                         rows_a,
@@ -182,7 +208,7 @@ impl<'base, T: ArrayType, const NDIMS: usize> Array<'base, T, NDIMS> {
                 }
 
                 Ok(ArrayBase {
-                    shape: Shape::new([cols_a; min_const(NDIMS, NDIMS2)]),
+                    dims: [cols_a; min_const(NDIMS, NDIMS2)],
                     data: (self.data.chunks_exact(cols_a).zip(repeat(other.data)).map(
                         |(a_row, b_col)| {
                             a_row
@@ -201,8 +227,8 @@ impl<'base, T: ArrayType, const NDIMS: usize> Array<'base, T, NDIMS> {
             }
             (1, 1) => {
                 // vector vector
-                let len_a = self.shape.dims()[0];
-                let len_b = other.shape.dims()[0];
+                let len_a = self.dims[0];
+                let len_b = other.dims[0];
                 if len_a != len_b {
                     return Err(Error::MatMul {
                         rows_a: len_a,
@@ -211,7 +237,7 @@ impl<'base, T: ArrayType, const NDIMS: usize> Array<'base, T, NDIMS> {
                 }
 
                 Ok(ArrayBase {
-                    shape: Shape::new([1; min_const(NDIMS, NDIMS2)]), // always 1 length
+                    dims: [1; min_const(NDIMS, NDIMS2)], // always 1 length
                     data: vec![self
                         .data
                         .iter()
@@ -225,7 +251,7 @@ impl<'base, T: ArrayType, const NDIMS: usize> Array<'base, T, NDIMS> {
     }
 }
 
-pub trait ArrayType:
+pub trait ArrType:
     Copy
     + PartialEq
     + Debug
--- src/tests.rs
+++ src/tests.rs
@@ -1,292 +1,323 @@
 mod array_tests {
+    use crate::array::ArrResult;
+
     #[test]
-    fn eq1() {
+    fn eq1() -> ArrResult<()> {
         arrs!(let arr1 = Array([2,2], [0,1,2,3]));
         arrs!(let arr2 = Array([2,2], [0,1,2,3]));
 
-        assert_eq!(arr1, arr2)
+        Ok(assert_eq!(arr1, arr2))
     }
 
     #[test]
-    fn eq2() {
+    fn eq2() -> ArrResult<()> {
         arrs!(let arr1 = [15]);
         arrs!(let arr2 = Array([1], [15]));
 
-        assert_eq!(arr1, arr2)
+        Ok(assert_eq!(arr1, arr2))
     }
 
     #[test]
-    fn eq3() {
+    fn eq3() -> ArrResult<()> {
         arrs!(let arr1 = [1,2,3]);
         arrs!(let arr2 = Array([3], [1,2,3]));
 
-        assert_eq!(arr1, arr2)
+        Ok(assert_eq!(arr1, arr2))
     }
 
     #[test]
-    fn add1() {
+    fn add1() -> ArrResult<()> {
         arrs!(let arr1 = [10]);
         arrs!(let arr2 = Array([2,2], [3,2,1,0]));
 
         arrs!(let expected = Array([2,2], [13, 12, 11, 10]));
         arrs!(let actual = add(&arr1, &arr2));
 
-        assert_eq!(expected, actual);
+        Ok(assert_eq!(expected, actual))
     }
 
     #[test]
-    fn mul1() {
+    fn mul1() -> ArrResult<()> {
         arrs!(let arr1 = [10]);
         arrs!(let arr2 = Array([4], [0, 1, 2, 3]));
 
         arrs!(let expected = Array([4], [0, 10, 20, 30]));
         arrs!(let actual = mul(&arr1, &arr2));
 
-        assert_eq!(expected, actual);
+        Ok(assert_eq!(expected, actual))
     }
 
     #[test]
-    fn mul2() {
+    fn mul2() -> ArrResult<()> {
         arrs!(let arr1 = [10]);
         arrs!(let arr2 = Array([2,2], [0,1,2,3]));
 
         arrs!(let expected = Array([2,2], [0,10,20,30]));
         arrs!(let actual = mul(&arr1, &arr2));
 
-        assert_eq!(expected, actual);
+        Ok(assert_eq!(expected, actual))
     }
 
     #[test]
-    fn mul3() {
+    fn mul3() -> ArrResult<()> {
         arrs!(let arr1 = Array([2], [0, 1]));
         arrs!(let arr2 = Array([2, 3], [0, 1, 2, 3, 4, 5]));
 
         arrs!(let expected = Array([2, 3], [0, 1, 0, 3, 0, 5]));
         arrs!(let actual = mul(&arr1, &arr2));
 
-        assert_eq!(expected, actual);
+        Ok(assert_eq!(expected, actual))
     }
 
     #[test]
-    fn mul4() {
+    fn mul4() -> ArrResult<()> {
         arrs!(let arr1 = Array([2, 2, 2], [0, 1, 2, 3, 4, 5, 6, 7]));
         arrs!(let arr2 = Array([1, 2], [0, 1]));
 
         arrs!(let expected = Array([2, 2, 2], [0, 0, 2, 3, 0, 0, 6, 7]));
         arrs!(let actual = mul(&arr1, &arr2));
 
-        assert_eq!(expected, actual);
+        Ok(assert_eq!(expected, actual))
     }
 
     #[test]
-    fn mul6() {
+    fn mul6() -> ArrResult<()> {
         arrs!(let arr1 = Array([2, 2, 2], [0, 1, 2, 3, 4, 5, 6, 7]));
         arrs!(let arr2 = Array([1, 2], [0, 1]));
 
         arrs!(let expected = Array([2, 2, 2], [0, 0, 2, 3, 0, 0, 6, 7]));
         arrs!(let actual = mul(&arr1, &arr2));
 
-        assert_eq!(expected, actual);
+        Ok(assert_eq!(expected, actual))
     }
 
     #[test]
-    fn add2() {
+    fn add2() -> ArrResult<()> {
         arrs!(let arr1 = Array([2,2], [0,1,2,3]));
         arrs!(let arr2 = Array([2,2], [3,2,1,0]));
 
         arrs!(let expected = Array([2,2], [3,3,3,3]));
         arrs!(let actual = add(&arr1, &arr2));
 
-        assert_eq!(expected, actual);
+        Ok(assert_eq!(expected, actual))
     }
 
     #[test]
-    fn add3() {
+    fn add3() -> ArrResult<()> {
         arrs!(let arr1 = [10]);
         arrs!(let arr2 = Array([2,2], [12,13,14,15]));
 
         arrs!(let expected = Array([2,2], [22,23,24,25]));
         arrs!(let actual = add(&arr1, &arr2));
 
-        assert_eq!(expected, actual);
+        Ok(assert_eq!(expected, actual))
     }
 
     #[test]
-    fn sub1() {
+    fn sub1() -> ArrResult<()> {
         arrs!(let arr1 = Array([2,2], [0,1,2,3]));
         arrs!(let arr2 = Array([2,2], [3,2,1,0]));
 
         arrs!(let expected = Array([2,2], [-3,-1,1,3]));
         arrs!(let actual = sub(&arr1, &arr2));
 
-        assert_eq!(expected, actual);
+        Ok(assert_eq!(expected, actual))
     }
 
     #[test]
-    fn sub2() {
+    fn sub2() -> ArrResult<()> {
         arrs!(let arr1 = [10]);
         arrs!(let arr2 = Array([2,2], [12,13,14,15]));
 
         arrs!(let expected = Array([2,2], [-2,-3,-4,-5]));
         arrs!(let actual = sub(&arr1, &arr2));
 
-        assert_eq!(expected, actual);
+        Ok(assert_eq!(expected, actual))
     }
 
     #[test]
-    fn vec_dot_vec() {
+    fn vec_dot_vec() -> ArrResult<()> {
         arrs!(let vec1 = [3,4,5]);
         arrs!(let vec2 = [1,0,1]);
 
         arrs!(let expected = [8]);
         arrs!(let actual = matmul(&vec1, &vec2));
-        assert_eq!(expected, actual)
+
+        Ok(assert_eq!(expected, actual))
     }
 
     #[test]
-    fn mat_mul_vec() {
+    fn mat_mul_vec() -> ArrResult<()> {
         arrs!(let mat1 = Array([3,3], [1,2,1,2,3,2,1,2,1]));
         arrs!(let vec1 = [4,4,4]);
 
         arrs!(let expected = [16,28,16]);
         arrs!(let actual = matmul(&mat1, &vec1));
-        assert_eq!(expected, actual)
+
+        Ok(assert_eq!(expected, actual))
     }
 }
 
-mod shape_tests {
-    use crate::array::Error;
-    use crate::array::Shape;
+// mod shape_tests {
+//     use crate::array::{ArrResult, Error, Shape};
 
-    #[test]
-    fn test_eq1() {
-        let a = Shape::new([3, 4, 5]);
-        let b = Shape::new([3, 4, 5]);
-        assert_eq!(a, b);
-    }
+//     #[test]
+//     fn test_eq1() -> ArrResult<()> {
+//         let a = Shape::new([3, 4, 5])?;
+//         let b = Shape::new([3, 4, 5])?;
 
-    #[test]
-    fn test_eq2() {
-        let a = Shape::new([1000, 1000, 1000]);
-        let b = Shape::new([1000, 1000, 1000]);
-        assert_eq!(a, b);
-    }
+//         Ok(assert_eq!(a, b))
+//     }
 
-    #[test]
-    fn test_ne1() {
-        let a = Shape::new([1000, 1000, 1000]);
-        let b = Shape::new([1001, 1000, 1000]);
-        assert_ne!(a, b);
-    }
+//     #[test]
+//     fn test_eq2() -> ArrResult<()> {
+//         let a = Shape::new([1000, 1000, 1000])?;
+//         let b = Shape::new([1000, 1000, 1000])?;
 
-    #[test]
-    fn test_ne2() {
-        let a = Shape::new([1, 2, 3]);
-        let b = Shape::new([1, 2, 4]);
-        assert_ne!(a, b);
-    }
+//         Ok(assert_eq!(a, b))
+//     }
 
-    #[test]
-    fn test_cast_ok1() {
-        let a = Shape::new([3, 256, 256]);
-        let b = Shape::new([3]);
-        let actual = a.broadcast(&b).unwrap().0;
-        let expected = Shape::new([3, 256, 256]);
-        assert_eq!(expected, actual);
-    }
+//     #[test]
+//     fn test_ne1() -> ArrResult<()> {
+//         let a = Shape::new([1000, 1000, 1000])?;
+//         let b = Shape::new([1001, 1000, 1000])?;
 
-    #[test]
-    fn test_cast_ok2() {
-        let a = Shape::new([1, 6, 1, 8]);
-        let b = Shape::new([5, 1, 7]);
-        let actual = a.broadcast(&b).unwrap().0;
-        let expected = Shape::new([5, 6, 7, 8]);
-        assert_eq!(expected, actual);
-    }
+//         Ok(assert_ne!(a, b))
+//     }
 
-    #[test]
-    fn test_cast_ok3() {
-        let a = Shape::new([4, 5]);
-        let b = Shape::new([1]);
-        let actual = a.broadcast(&b).unwrap().0;
-        let expected = Shape::new([4, 5]);
-        assert_eq!(expected, actual);
-    }
+//     #[test]
+//     fn test_ne2() -> ArrResult<()> {
+//         let a = Shape::new([1, 2, 3])?;
+//         let b = Shape::new([1, 2, 4])?;
 
-    #[test]
-    fn test_cast_ok4() {
-        let a = Shape::new([4, 5]);
-        let b = Shape::new([4]);
-        let actual = a.broadcast(&b).unwrap().0;
-        let expected = Shape::new([4, 5]);
-        assert_eq!(expected, actual);
-    }
+//         Ok(assert_ne!(a, b))
+//     }
 
-    #[test]
-    fn test_cast_ok5() {
-        let a = Shape::new([5, 3, 15]);
-        let b = Shape::new([5, 1, 15]);
-        let actual = a.broadcast(&b).unwrap().0;
-        let expected = Shape::new([5, 3, 15]);
-        assert_eq!(expected, actual);
-    }
+//     #[test]
+//     fn test_cast_ok1() -> ArrResult<()> {
+//         let a = Shape::new([3, 256, 256])?;
+//         let b = Shape::new([3])?;
 
-    #[test]
-    fn test_cast_ok6() {
-        let a = Shape::new([5, 3, 15]);
-        let b = Shape::new([5, 3]);
-        let actual = a.broadcast(&b).unwrap().0;
-        let expected = Shape::new([5, 3, 15]);
-        assert_eq!(expected, actual);
-    }
+//         let actual = a.broadcast(&b).unwrap().0;
+//         let expected = Shape::new([3, 256, 256])?;
 
-    #[test]
-    fn test_cast_ok7() {
-        let a = Shape::new([5, 3, 15]);
-        let b = Shape::new([1, 3]);
-        let actual = a.broadcast(&b).unwrap().0;
-        let expected = Shape::new([5, 3, 15]);
-        assert_eq!(expected, actual);
-    }
+//         Ok(assert_eq!(expected, actual))
+//     }
 
-    #[test]
-    fn test_cast_err1() {
-        let a = Shape::new([3]);
-        let b = Shape::new([4]);
-        let actual = a.broadcast(&b);
-        let expected = Error::Broadcast {
-            dims1: a.to_vec(),
-            dims2: b.to_vec(),
-        };
-        assert_eq!(expected, actual.unwrap_err());
-    }
+//     #[test]
+//     fn test_cast_ok2() -> ArrResult<()> {
+//         let a = Shape::new([1, 6, 1, 8])?;
+//         let b = Shape::new([5, 1, 7])?;
 
-    #[test]
-    fn test_cast_err2() {
-        let a = Shape::new([1, 2]);
-        let b = Shape::new([3, 4, 8]);
-        let actual = a.broadcast(&b);
-        let expected = Error::Broadcast {
-            dims1: a.to_vec(),
-            dims2: b.to_vec(),
-        };
-        assert_eq!(expected, actual.unwrap_err());
-    }
+//         let actual = a.broadcast(&b).unwrap().0;
+//         let expected = Shape::new([5, 6, 7, 8])?;
 
-    #[test]
-    fn test_volume1() {
-        let a = Shape::new([3, 256, 256]);
-        let expected = 3 * 256 * 256;
-        assert_eq!(expected, a.volume());
-    }
+//         Ok(assert_eq!(expected, actual))
+//     }
 
-    #[test]
-    fn test_volume2() {
-        let a = Shape::new([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-        let expected = (1..10).product::<usize>();
-        assert_eq!(expected, a.volume());
-    }
-}
+//     #[test]
+//     fn test_cast_ok3() -> ArrResult<()> {
+//         let a = Shape::new([4, 5])?;
+//         let b = Shape::new([1])?;
+
+//         let actual = a.broadcast(&b).unwrap().0;
+//         let expected = Shape::new([4, 5])?;
+
+//         Ok(assert_eq!(expected, actual))
+//     }
+
+//     #[test]
+//     fn test_cast_ok4() -> ArrResult<()> {
+//         let a = Shape::new([4, 5])?;
+//         let b = Shape::new([4])?;
+
+//         let actual = a.broadcast(&b).unwrap().0;
+//         let expected = Shape::new([4, 5])?;
+
+//         Ok(assert_eq!(expected, actual))
+//     }
+
+//     #[test]
+//     fn test_cast_ok5() -> ArrResult<()> {
+//         let a = Shape::new([5, 3, 15])?;
+//         let b = Shape::new([5, 1, 15])?;
+
+//         let actual = a.broadcast(&b).unwrap().0;
+//         let expected = Shape::new([5, 3, 15])?;
+
+//         Ok(assert_eq!(expected, actual))
+//     }
+
+//     #[test]
+//     fn test_cast_ok6() -> ArrResult<()> {
+//         let a = Shape::new([5, 3, 15])?;
+//         let b = Shape::new([5, 3])?;
+
+//         let actual = a.broadcast(&b).unwrap().0;
+//         let expected = Shape::new([5, 3, 15])?;
+
+//         Ok(assert_eq!(expected, actual))
+//     }
+
+//     #[test]
+//     fn test_cast_ok7() -> ArrResult<()> {
+//         let a = Shape::new([5, 3, 15])?;
+//         let b = Shape::new([1, 3])?;
+
+//         let actual = a.broadcast(&b).unwrap().0;
+//         let expected = Shape::new([5, 3, 15])?;
+
+//         Ok(assert_eq!(expected, actual))
+//     }
+
+//     #[test]
+//     fn test_cast_err1() -> ArrResult<()> {
+//         let a = Shape::new([3])?;
+//         let b = Shape::new([4])?;
+
+//         let actual = a.broadcast(&b).unwrap_err();
+//         let expected = Error::Broadcast {
+//             dims1: a.to_vec(),
+//             dims2: b.to_vec(),
+//         };
+
+//         Ok(assert_eq!(expected, actual))
+//     }
+
+//     #[test]
+//     fn test_cast_err2() -> ArrResult<()> {
+//         let a = Shape::new([1, 2])?;
+//         let b = Shape::new([3, 4, 8])?;
+
+//         let actual = a.broadcast(&b).unwrap_err();
+//         let expected = Error::Broadcast {
+//             dims1: a.to_vec(),
+//             dims2: b.to_vec(),
+//         };
+
+//         Ok(assert_eq!(expected, actual))
+//     }
+
+//     #[test]
+//     fn test_volume1() -> ArrResult<()> {
+//         let a = Shape::new([3, 256, 256])?;
+
+//         let expected = 3 * 256 * 256;
+//         let actual = a.volume();
+
+//         Ok(assert_eq!(expected, actual))
+//     }
+
+//     #[test]
+//     fn test_volume2() -> ArrResult<()> {
+//         let a = Shape::new([1, 2, 3, 4, 5, 6, 7, 8, 9])?;
+
+//         let expected = (1..10).product::<usize>();
+//         let actual = a.volume();
+
+//         Ok(assert_eq!(expected, actual))
+//     }
+// }
 
 /*
 mod derank_slice_tests {
@@ -381,19 +412,20 @@ mod derank_slice_tests {
 
 /*
 mod array_idx_tests {
-    use crate::array::Shape;
+    use crate::array::{ArrResult, Shape};
 
     #[test]
-    fn read_correct_ndims() {
+    fn read_correct_ndims() -> ArrResult<()> {
         arrs!(let _test_imgs = IDX("idx-files/t10k-images-idx3-ubyte"));
+        Ok(())
     }
 
     #[test]
-    fn read_correct_dims() {
+    fn read_correct_dims() -> ArrResult<()> {
         arrs!(let test_imgs = IDX("idx-files/t10k-images-idx3-ubyte"));
-        let expected = Shape::new([10000, 28, 28]);
+        let expected = Shape::new([10000, 28, 28])?;
 
-        assert_eq!(&expected, test_imgs.shape())
+        Ok(assert_eq!(&expected, test_imgs.shape()))
     }
 }
 
